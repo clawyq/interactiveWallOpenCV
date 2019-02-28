cbf6803e4109e2a2f6a80c9a87bf1ddb93939b88
LAPTOP-E3JOI16D\clint <clawyq@hotmail.com>
2019-02-28 17:20:26 +0800

    Fixed window for message sending and refactored code for readability

--- test.py
+++ test.py
@@ -1,70 +1,65 @@
 import cv2
-import numpy as np
 
-eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
+XSTART = 100
+XEND = 400
+YSTART = 250
+YEND = 750
+LOWER_BRIGHTNESS_THRESHOLD = 60
+KERNEL_BLUR = 13
+STANDARD_DEVIATION_X = 0
+GREEN_BOX_RGB = (0, 255, 0)
+RESOLUTION_SCALE = 1
+MIN_DISTANCE = 50
+EYE_CASCADE_CLASSIFIER = 'haarcascade_righteye_2splits.xml'
 
-# cap = cv2.VideoCapture("eye_recording.mp4")
-cap = cv2.VideoCapture(0)
-#cap.set(CV_CAP_PROP_FRAME_WIDTH, 352)
-#cap.set(CV_CAP_PROP_FRAME_HEIGHT, 288)
+# value to be passed into Canny() higher values means less edges will be detected
+EDGE_STRICTNESS = 52
+# higher values means an object must have more edges to shape it like a circle to be considered a circle
+CIRCLE_STRICTNESS = 16
+
+eye_cascade = cv2.CascadeClassifier(EYE_CASCADE_CLASSIFIER)
+
+cap = cv2.VideoCapture(0) # 0 defaults to the standard webcam
 
 while True:
     ret, frame = cap.read()
     if ret is False:
         break
 
-    XSTART = 100
-    XEND = 400
-    roi = frame[XSTART: XEND, 250: 750]
-    rows, cols, _ = roi.shape
-    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    blurg_roi = cv2.GaussianBlur(gray_roi, (13, 13), 0)
+    window = set()
+    for i in range(10):
 
-    _, bthreshold = cv2.threshold(blurg_roi, 60, 255, cv2.THRESH_BINARY_INV)
-    _,contours, _ = cv2.findContours(bthreshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-    eyes = eye_cascade.detectMultiScale(blurg_roi)
+        roi = frame[XSTART: XEND, YSTART: YEND]
+        rows, cols, _ = roi.shape
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        blurg_roi = cv2.GaussianBlur(gray_roi, (KERNEL_BLUR, KERNEL_BLUR), STANDARD_DEVIATION_X)
 
-    for eye in eyes:
-        window = set()
-        for i in range(3000):
-    #     (x, y, w, h) = cv2.boundingRect(cnt) 
+        _, bthreshold = cv2.threshold(blurg_roi, LOWER_BRIGHTNESS_THRESHOLD, 255, cv2.THRESH_BINARY_INV)
+
+        # RETR_TREE calculates full hierarchy of the contours for nested object detection
+        contours, _ = cv2.findContours(bthreshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        eyes = eye_cascade.detectMultiScale(blurg_roi)
+
+        for eye in eyes:
             (x, y, w, h) = eye
-            lrc = (x*3)//(XEND - XSTART)
+            lrc = (x * 3)//(XEND - XSTART)
             window.add(lrc)
             print('eye x:{} w:{}'.format(x, w))
-            cv2.rectangle(roi,(x,y),(x+w,y+h),(0,255,0),2)
-            roi_gray2 = blurg_roi[y:y+h, x:x+w]
-            roi_color2 = roi[y:y+h, x:x+w]
-            circles = cv2.HoughCircles(roi_gray2, cv2.HOUGH_GRADIENT, 1, 46, param1=52,
-	    	param2=16, minRadius=0,
-            maxRadius=0)
-            print(lrc)
-        for i in window:
-            print("set: {}".format(i))
-       # try:
-       #     for i in circles[0,:]:
-      #          print('{} {} {}'.format(i[0], i[1], i[2]))
-     #           xPosition = i[0]
-    #            segmentWidth = w/3
-             #   lrc = xPosition//segmentWidth
-        #        cv2.circle(roi_color2,(i[0], i[1]), i[2], (255,255,255) ,2)
-         #       cv2.circle(roi_color2, (i[0], i[1]), 2, (255, 255, 255), 3)
-         #   print("circle w: {}, h:{} s:{}".format(w, h, lrc) )
-#
-       # except Exception as e:
-        #    print("hello")
-         #   print(e)
-    #    cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 0, 255), 1)
-    #    cv2.line(roi, (x + int(w / 2), 0), (x + int(w / 2), rows), (255, 0, 0), 1)
-    #    cv2.line(roi, (0, y + int(h / 2)), (cols, y + int(h / 2)), (0, 255, 0), 1)
-    #    break
+            cv2.rectangle(roi, (x, y), (x + w, y + h), GREEN_BOX_RGB , 2)
+            roi_gray2 = blurg_roi[y : y + h, x : x + w]
+            roi_color2 = roi[y: y + h, x: x + w]
+            circles = cv2.HoughCircles(roi_gray2, cv2.HOUGH_GRADIENT, RESOLUTION_SCALE, MIN_DISTANCE,
+                                       param1=EDGE_STRICTNESS, param2=CIRCLE_STRICTNESS, minRadius=0, maxRadius=0)
 
-    cv2.imshow("roi", roi)
-    cv2.imshow("btresh", bthreshold)
-    key = cv2.waitKey(30)
-    if key == 27:
-        cap.release()
-        break
+        cv2.imshow("roi", roi)
+        cv2.imshow("btresh", bthreshold)
+        key = cv2.waitKey(1)
+        if key == 27:
+            cap.release()
+            break
+    for i in window:
+        print("window content: {}".format(i))
 
 cv2.destroyAllWindows()
